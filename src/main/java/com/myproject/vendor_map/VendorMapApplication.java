04d198ba00b0bb6733e1f550090f7e8d295bc52b
Glory0206 <dmb07223@gmail.com>
2025-11-19 16:57:41 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/myproject/vendor_map/VendorMapApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.vendor_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorMapApplication.class, args);
+	}
+
+}
